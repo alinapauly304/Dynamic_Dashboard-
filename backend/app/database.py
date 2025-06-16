add11677e0ad34f0c00315cb3e2c6ff4dfeb2e7e
alinapauly304 <alinapauly304@gmail.com>
2025-06-16 15:07:37 +0530

    correctons-week1

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import urllib.parse
 
-DATABASE_URL = "postgresql://dashboard_user:Alinapauly304@localhost/dashboard_db"
+
+username = "dashboard_user"
+password = "Alinapauly@304"  
+host = "localhost"
+database_name = "dashboard_db"
+
+encoded_password = urllib.parse.quote_plus(password)
+
+DATABASE_URL = f"postgresql://{username}:{encoded_password}@{host}/{database_name}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- backend/app/main.py
+++ backend/app/main.py
@@ -7,18 +7,20 @@ from sqlmodel import Field, SQLModel, Session, create_engine, select,text
 from app.database import engine
 from app.models import users, project, permission
 from passlib.context import CryptContext
-
+from pydantic import BaseModel
 from datetime import datetime, timedelta
 
 from app.utils.hashing import hash_password,verify_password
 from app.utils.jwt import  create_token,decode_token
+   
+from sqlalchemy.exc import SQLAlchemyError
+
+from app.config import origins
 
 Base.metadata.create_all(bind=engine)
 
 app=FastAPI()
-origins = [
-    "http://localhost:3000",
-]
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -28,60 +30,116 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
+@app.get("/health", tags=["Health Check"])
+def health_check():
+    """_summary_
 
-def read_root():
-    return{"message":"hello World"}
+    Returns:
+        _type_: _description_
+    """
+    return {"status": "ok", "message": "Server is running"}
+
+
+class Login_item(BaseModel):
+    username:str
+    password:str
+
+class LoginResponse(BaseModel):
+    access_token: str
+    token_type: str
+    role_id: int
+
+@app.post("/login",response_model=LoginResponse)
+def login(login:Login_item):
+    """_summary_
+
+    Args:
+        username (str, optional): _description_. Defaults to Body(...).
+        password (str, optional): _description_. Defaults to Body(...).
+
+    Raises:
+        HTTPException: _description_
+        HTTPException: _description_
+        HTTPException: _description_
+
+    Returns:
+        _type_: _description_
+    """
+    try:
+        with Session(engine) as session:
+            user_obj = session.exec(
+                select(users.User).where(users.User.username == login.username)
+            ).first()
+
+            if not user_obj or not verify_password(login.password, user_obj.password_hash):
+                raise HTTPException(status_code=401, detail="Invalid credentials")
+
+            token = create_token({"sub": user_obj.username})
+            return {
+                "access_token": token,
+                "token_type": "bearer",
+                "role_id": user_obj.role_id
+            }
+
+    except SQLAlchemyError as db_error:
+        raise HTTPException(status_code=500, detail="Database error")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
 
 
 
-@app.post("/login")
-def login(username: str = Body(...), password: str = Body(...)):
+class RegisterItem(BaseModel):
+    username: str 
+    email: str
+    password: str 
+    role_name: str 
+    organization_name: str 
 
-    with Session(engine) as session:
-        user_obj=session.exec(select(users.User).where(users.User.username == username)).first()
-
-        if not user_obj or not verify_password(password, user_obj.password_hash):
-            raise HTTPException(status_code=401, detail="Invalid credentials")
-
-        token = create_token({"sub": user_obj.username})
-        return {"access_token": token, "token_type": "bearer" ,"role_id":user_obj.role_id}
-    
 @app.post("/register")
-def register(
-    username: str = Body(...),
-    email: str = Body(...),
-    password: str = Body(...),
-    role_name: str = Body(...),
-    organization_name: str = Body(...)):
+def register(registerUser:RegisterItem):
+    
+    try:
+        with Session(engine) as session:
+            existing = session.exec(
+                select(users.User).where(users.User.username ==registerUser. username)
+            ).first()
+            if existing:
+                raise HTTPException(status_code=400, detail="Username already taken")
 
-    with Session(engine) as session:
-       
-        existing = session.exec(select(users.User).where(users.User.username == username)).first()
-        if existing:
-            raise HTTPException(status_code=400, detail="Username already taken")
-        
-        role_obj = session.exec(select(users.Role).where(users.Role.name == role_name)).first()
-        if not role_obj:
-            raise HTTPException(status_code=400, detail="Invalid role")
-        org_obj = session.exec(select(users.Organization).where(users.Organization.name == organization_name)).first()
-        if not org_obj:
-            raise HTTPException(status_code=400, detail="Invalid organization")
+            role_obj = session.exec(
+                select(users.Role).where(users.Role.name == registerUser.role_name)
+            ).first()
+            if not role_obj:
+                raise HTTPException(status_code=400, detail="Invalid role")
 
-        new_user = users.User(
-        username=username,
-        email=email,
-        password_hash=hash_password(password),
-        role_id=role_obj.id,
-        organization_id=org_obj.id,
-        created_at=datetime.utcnow(),
-        is_active=True
-    )
-        session.add(new_user)
-        session.commit()
-        session.refresh(new_user)
-        return {"message": "User registered", "username": new_user.username, "role": role_obj.name}
+            org_obj = session.exec(
+                select(users.Organization).where(users.Organization.name == registerUser.organization_name)
+            ).first()
+            if not org_obj:
+                raise HTTPException(status_code=400, detail="Invalid organization")
 
+            new_user = users.User(
+                username=registerUser.username,
+                email=registerUser.email,
+                password_hash=hash_password(registerUser.password),
+                role_id=role_obj.id,
+                organization_id=org_obj.id,
+                created_at=datetime.utcnow(),
+                is_active=True
+            )
+            session.add(new_user)
+            session.commit()
+            session.refresh(new_user)
 
+            return {
+                "message": "User registered",
+                "username": new_user.username,
+                "role": role_obj.name
+            }
 
+    except SQLAlchemyError as db_error:
+        raise HTTPException(status_code=500, detail="Database error")
 
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
